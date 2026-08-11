ea984a64d0ef300ad4bc1256e0ffc44204bd4614
Vishal R <vishalramesh1721@gmail.com>
2026-08-11 10:48:58 +0530

    Update api.py

--- apps/practice_app/practice_app/api.py
+++ apps/practice_app/practice_app/api.py
@@ -9,42 +9,42 @@ def task(task_subject):
     doc.insert()
     return doc.name
     
-@frappe.whitelist()
-def demo_progress():
-    for i in range(10):
-        frappe.publish_progress(
-            (i + 1) * 10,
-            title="Import",
-            description=f"Processing {i+1}/10"
-        )
-        time.sleep(1)
+# @frappe.whitelist()
+# def demo_progress():
+#     for i in range(10):
+#         frappe.publish_progress(
+#             (i + 1) * 10,
+#             title="Import",
+#             description=f"Processing {i+1}/10"
+#         )
+#         time.sleep(1)
 
-    return "Done"
-
-
-@frappe.whitelist()
-def start_short_job():
-    frappe.enqueue(
-    "practice_app.api.generate_pdf",
-    queue="short"
-)
-
-    return "Short job enqueued"
+#     return "Done"
 
 
-def generate_pdf():
-    print("Short job")
+# @frappe.whitelist()
+# def start_short_job():
+#     frappe.enqueue(
+#     "practice_app.api.generate_pdf",
+#     queue="short"
+# )
+
+#     return "Short job enqueued"
 
 
-@frappe.whitelist()
-def start_long_job():
-    frappe.enqueue(
-        "practice_app.api.long_job",
-        queue="long"
-    )
-
-    return "Long job enqueued"
+# def generate_pdf():
+#     print("Short job")
 
 
-def long_job():
-    frappe.msgprint("Long job")
+# @frappe.whitelist()
+# def start_long_job():
+#     frappe.enqueue(
+#         "practice_app.api.long_job",
+#         queue="long"
+#     )
+
+#     return "Long job enqueued"
+
+
+# def long_job():
+#     frappe.msgprint("Long job")
